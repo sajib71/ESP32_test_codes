a560a109613f9ff676e688e9e72c87db455d2936
Sanaullah Sajib <sajib71208@gmail.com>
2025-03-09 23:46:55 +0600

    use lastButtonState && currentButtonState method.

--- Read_Button/Read_Button.ino
+++ Read_Button/Read_Button.ino
@@ -6,26 +6,33 @@
  * Author: Sanaullah Sajib
  * Date: March 9, 2025
  */
-int ledPin = 4;                      // LED connected to GPIO 4
-int buttonPin = 35;                  // Button connected to GPIO 35
-int buttonState;                     // Toggle state
+int ledPin = 4;                       // LED connected to GPIO 4
+int buttonPin = 35;                   // Button connected to GPIO 35
+// int buttonState;                   // Stores the button state
+bool buttonState = false;             // LED toggle state (ON/OFF)
+bool lastButtonState = HIGH;          // Stores the previous button state
 
 void setup() {
   Serial.begin(115200);
-  pinMode(buttonPin, INPUT);         // Use pull-up resistor
-  pinMode(ledPin, OUTPUT);           // Use for Output
+  pinMode(buttonPin, INPUT);          // Use pull-up resistor
+  pinMode(ledPin, OUTPUT);            // Use for Output
 }
 
 void loop() {
   // Read the state of the pushbutton value
-  buttonState = digitalRead(buttonPin); 
+  // buttonState = digitalRead(buttonPin); 
+  bool currentButtonState = digitalRead(buttonPin);
   // Detect button press (only when transitioning from HIGH to LOW)
-  if(buttonState == LOW){
-    digitalWrite(ledPin, HIGH);     // Turn ON the led
-    Serial.println("Button is Pressed");
-  } else {
-    digitalWrite(ledPin, LOW);      // Turn OFF the led
-    Serial.println("Button is not Pressed");
+  if(lastButtonState == HIGH && currentButtonState == LOW){
+    buttonState = !buttonState;       // Toggle LED state
+    if(buttonState){
+      digitalWrite(ledPin, HIGH);
+      Serial.print("Button is Pressed: LED ON");
+    } else{
+      digitalWrite(ledPin, LOW);
+      Serial.println("Button is not Pressed: LED OFF");
+    }
+    delay(200);  // Debounce delay
   }
-  delay(50);  // Delay to reduce rapid polling and debounce the button
+  lastButtonState = currentButtonState;  // Update button state for next loop
 }
